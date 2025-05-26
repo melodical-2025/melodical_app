4e1167b2fda4fcf777d171961b670348ca009ece
yyj1001 <yeinjung1001@gmail.com>
2025-05-26 19:30:08 +0900

    평가페이지 구조변경 위젯으로 분리함

---
+++ lib/widgets/ratemusictab.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RateMusicTab extends StatelessWidget {
+  const RateMusicTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('음악 평가 기능은 준비 중입니다.'),
+    );
+  }
+}
